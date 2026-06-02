5f88300539246286f6c23642626b98b4a28aff95
AhmadHarun <harunxcode@gmail.com>
2026-06-02 14:46:31 +0700

    first commit

--- lib/halama3.dart
+++ lib/halama3.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class HalamanKetiga extends StatelessWidget {
-  const HalamanKetiga({super.key});
+// class HalamanKetiga extends StatelessWidget {
+//   const HalamanKetiga({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('halaman ketiga'),
-        backgroundColor: Colors.teal,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('halaman ketiga'),
+//         backgroundColor: Colors.teal,
+//       ),
+//     );
+//   }
+// }
